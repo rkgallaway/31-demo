c2fccb8e2148bce512a62b02b964554b702407f9
Ryan Gallaway <rkgallaway@msn.com>
2022-08-30 00:12:25 -0700

    context settings mutable, UI additions

--- src/Context/Site/index.js
+++ src/Context/Site/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const SiteContext = React.createContext();
 
 function SiteProvider(props) {
+  const [title, setTitle] = useState('Daily Dose of Code');
+  const [twitter, setTwitter] = useState('DailyDoseOfCode');
 
-  const state = {
-    title: 'My Amazing Website',
-    twitter: 'amazingness',
-  };
+  const changeTitleAndTwitter = (str) => {
+    setTitle(str);
+    let handle = str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
+    setTwitter(handle);
+  }
+
+  let exportedValues = {
+    title,
+    twitter,
+    changeTitleAndTwitter,
+  }
 
   return (
-    <SiteContext.Provider value={state}>
+    <SiteContext.Provider value={exportedValues}>
       {props.children}
     </SiteContext.Provider>
   );
